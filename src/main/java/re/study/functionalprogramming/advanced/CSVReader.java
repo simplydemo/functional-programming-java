c48e6c737342646a715451abd5ffdd3e9237fb77
lamp.java <lamp.java@gmail.com>
2017-09-13 15:40:46 +0900

    CSV 파일을 트랜스포메이션 하는 map 함수 응용 추가 CSVReader.toListWithFunction 함수 참고.

--- src/main/java/re/study/functionalprogramming/advanced/CSVReader.java
+++ src/main/java/re/study/functionalprogramming/advanced/CSVReader.java
@@ -1,10 +1,13 @@
 package re.study.functionalprogramming.advanced;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -43,6 +46,107 @@ public class CSVReader {
             }
             return map;
         }).collect(Collectors.toList());
+        return values;
+    }
+
+    // Function<? super T, ? extends Stream<? extends R>> mapper
+    static public <T> List<T> toListMap(final Path path, final Function<String, ? extends Stream<T>> mapper)
+            throws IOException {
+        // final String[] keys = Files.lines(path).findFirst().get().split(",");
+        // @formatter:off
+        List<T> values = Files.lines(path)
+                // .skip(1)
+                .flatMap(mapper)
+                .collect(Collectors.toList());
+        // @formatter:on 
+        return values;
+    }
+
+    /**
+     * @param keys 사전에정의된 HeaderNames
+     * @return List<Arrays> 데이터
+     */
+    private final static Function<String[], List<String[]>> buildArraysList(final String[] keys) {
+        return v -> {
+            final List<String[]> list = new ArrayList<>();
+            final String account = v[0];
+            final String mapping = v[1];
+            final String ptype = v[2];
+            for (int i = 3; i < v.length; i++) {
+                String[] data = new String[5];
+                data[0] = account;
+                data[1] = mapping;
+                data[2] = ptype;
+                data[3] = keys[i]; // week
+                data[4] = v[i]; // value of week
+                // String c = Arrays.toString(data);
+                // System.out.printf("\n %d ==> %s ", i, c);
+                list.add(data);
+            }
+            return list;
+        };
+    };
+
+    static public List<List<String[]>> toListWithFunction(final Path path, final String dilim) throws IOException {
+        // @formatter:off
+        final String[] keys = Files.lines(path).findFirst().map(s -> s.split(",")).get();
+        final List<List<String[]>> values = Files.lines(path)
+                                            .skip(1) /* 첫번째 row 는 헤더 이다. */
+                                            .map(line -> line.split(dilim))
+                                            .map(buildArraysList(keys))
+                                            .collect(Collectors.toList());
+        // @formatter:on
+        return values;
+    }
+
+    /**
+     * 
+     * <pre>
+     * - 1 행은 타이틀
+     * - 3 번째 인덱스부터 week 
+     * - 2행"ACC_GROUP" "Mapping" "Pricing Type" 기준의 그룹핑 데이타가 반복
+     * </pre>
+     * 
+     * <code>
+    원천 데이터
+    ACC_GROUP  Mapping Pricing Type    201721  201722  201723  201724 ...
+    BBY Item574 SRP 0   0   0   0 ...
+    BBY Item574 MAP 0   0   0   0 ...
+    BBY Item574 IR  0   0   0   0 ...
+    BBY Item574 Incr. IR    0   0   0   0 ...
+    BBY Item574 TC to Claim 0   0   0   0 ...
+    BBY Item574 MAP Compliant   0   0   0   0 ...
+    BBY Item574 Bundle  0   0   0   0 ...
+    
+    매핑기준
+    ACC_GROUP=BBY
+    Mapping=Item574
+    Pricing Type=SRP
+    Week=201721
+    Value=0
+    
+    ACC_GROUP=BBY
+    Mapping=Item574
+    Pricing Type=SRP
+    Week=201722
+    Value=5999 
+    </code>
+     * 
+     * @param path 파일경로
+     * @param dilim CSV 파일 구분자
+     * @param fmap 데이터 컨버저닝 사용자 함수
+     * @return
+     * @throws IOException
+     */
+    static public <T, R> List<R> toListWithFunction(final Path path, final String dilim,
+            Function<? super String[], ? extends R> fmap) throws IOException {
+        // @formatter:off
+        final List<R> values = Files.lines(path)
+                                            .skip(1) /* 첫번째 row 는 헤더 이다. */
+                                            .map(line -> line.split(dilim))
+                                            .map(fmap)
+                                            .collect(Collectors.toList());
+        // @formatter:on
         return values;
     }
 
--- src/main/java/re/study/functionalprogramming/advanced/CSVReaderTest.java
+++ src/main/java/re/study/functionalprogramming/advanced/CSVReaderTest.java
@@ -1,8 +1,13 @@
 package re.study.functionalprogramming.advanced;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -26,7 +31,7 @@ public class CSVReaderTest {
 
     protected void importFromFile() {
         try (Connection connection = getConnection()) {
-            String loadQuery = "LOAD DATA LOCAL INFILE '" + "C:\\upload.csv"
+            String loadQuery = "LOAD DATA LOCAL INFILE '" + "C:/upload.csv"
                     + "' INTO TABLE txn_tbl FIELDS TERMINATED BY ','"
                     + " LINES TERMINATED BY '\n' (txn_amount, card_number, terminal_id) ";
             System.out.println(loadQuery);
@@ -35,6 +40,21 @@ public class CSVReaderTest {
         }
         catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void closeQuitly(Connection conn, PreparedStatement pstmt) {
+        try {
+            pstmt.close();
+        }
+        catch (Exception e2) {
+
+        }
+        try {
+            conn.close();
+        }
+        catch (Exception e3) {
+
         }
     }
 
@@ -63,18 +83,7 @@ public class CSVReaderTest {
             e.printStackTrace();
         }
         finally {
-            try {
-                pstmt.close();
-            }
-            catch (Exception e2) {
-
-            }
-            try {
-                conn.close();
-            }
-            catch (Exception e3) {
-
-            }
+            closeQuitly(conn, pstmt);
         }
     }
 
@@ -114,12 +123,95 @@ public class CSVReaderTest {
     @Test
     public void test_importFcstMobileTrainningData() throws Exception {
         final String filename = "C:/Users/seonbo.shim/Documents/Working/AWS-ML-TRAINNING/FCST_MOBILE_SELLOUT.csv";
-        LocalDateTime localDtm = (LocalDateTime) DateMaps.ofNow().get();
         File f = new File(filename);
         List<List<String>> values = CSVReader.toList(f.toPath());
         String sql = "insert into IR_FCST_MOBILE (GC,AP2,AP1,ACCOUNT,MODEL,WEEKNUM,SELLOUT) values "
                 + "(?,?,?,?,?,?,?)";
         importData(values, sql);
+        LocalDateTime localDtm = (LocalDateTime) DateMaps.ofNow().get();
+        log.info("duration SECS: {}", DateMaps.of(localDtm).duration().getSeconds());
+
+    }
+
+    protected void importArrayData(List<List<String[]>> values, final String sql) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            int rows = 0;
+            for (List<String[]> list : values) {
+                for (String[] arr : list) {
+                    for (int i = 0; i < arr.length; i++) {
+                        pstmt.setString(i + 1, arr[i]);
+                    }
+                    pstmt.addBatch();
+                }
+                if (rows % 1999 == 0) {
+                    pstmt.executeBatch();
+                }
+                ++rows;
+            }
+            pstmt.executeBatch();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            closeQuitly(conn, pstmt);
+        }
+    }
+
+    /**
+     * @param keys 사전에정의된 HeaderNames
+     * @return List<Arrays> 데이터
+     */
+    private final Function<String[], List<String[]>> buildListMapFunc(final String[] keys) {
+        return v -> {
+            final List<String[]> list = new ArrayList<>();
+            final String account = v[0];
+            final String mapping = v[1];
+            final String ptype = v[2];
+            for (int i = 3; i < v.length; i++) {
+                String[] data = new String[5];
+                data[0] = account;
+                data[1] = mapping;
+                data[2] = ptype;
+                data[3] = keys[i]; // week
+                data[4] = v[i]; // value of week
+                // String c = Arrays.toString(data);
+                // System.out.printf("\n %d ==> %s ", i, c);
+                list.add(data);
+            }
+            return list;
+        };
+    };
+
+    @Test
+    public void ut1002_Map() throws Exception {
+        final String filename = "/samples-2.csv";
+        File f = new File(CSVReaderTest.class.getResource(filename).toURI());
+        log.info("f.exists(): {}", f.exists());
+        List<List<String[]>> values = CSVReader.toListWithFunction(f.toPath(), ",");
+        log.info("values.size(): {}", values.size());
+        for (List<String[]> rows : values) {
+            for (String[] arrs : rows) {
+                System.out.println(Arrays.toString(arrs));
+            }
+        }
+    }
+
+    @Test
+    public void ut1003_importIRPromotion() throws Exception {
+        final String filepath = "C:/Users/seonbo.shim/Documents/Working/FCST AWS-ML PoC/Promotion 20170822.csv";
+        final Path path = new File(filepath).toPath();
+        System.out.println("path.toFile().exists(): " + path.toFile().exists());
+        final String[] keys = Files.lines(path).findFirst().map(s -> s.split(",")).get();
+        List<List<String[]>> values = CSVReader.toListWithFunction(path, ",", buildListMapFunc(keys));
+        // log.info("values.size(): {}", values.size());
+        String sql = "insert into IR_PROMOTION (ACCOUNT,MODEL,PTYPE,WEEKNO,VALUE) values " + "(?,?,?,?,?)";
+        LocalDateTime localDtm = (LocalDateTime) DateMaps.ofNow().get();
+        importArrayData(values, sql);
         log.info("duration SECS: {}", DateMaps.of(localDtm).duration().getSeconds());
 
     }
